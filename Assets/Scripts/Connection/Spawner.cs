023c2f507977d8c45b4403451e99abb51452f290
loupancas <129621626+loupancas@users.noreply.github.com>
2024-11-09 23:36:34 -0500

    lobby

--- Assets/Scripts/Connection/Spawner.cs
+++ Assets/Scripts/Connection/Spawner.cs
@@ -3,74 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using Fusion.Sockets;
-using System.Collections;
 
 
-public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
+public class Spawner : MonoBehaviour
 {
     [SerializeField] private NetworkPrefabRef _playerPrefab;
     private List<Transform> _recentSpawnPoints = new List<Transform>();
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-       //var spawnPoint = PlayerSpawnPoint();
-       // if (runner.IsServer)
-       // {
-       //     runner.Spawn(_playerPrefab, spawnPoint.position, null, player);
-       // }
+        var spawnPoint = PlayerSpawnPoint();
+        if (runner.IsServer)
+        {
+            runner.Spawn(_playerPrefab, spawnPoint.position, null, player);
+        }
     }
 
-    //private CharacterInputHandler _characterInputHandler;
-    
-    //public void OnInput(NetworkRunner runner, NetworkInput input)
-    //{
-    //    if (!NetworkPlayer.Local) return;
+    private Player _characterInputHandler;
 
-    //    _characterInputHandler ??= NetworkPlayer.Local.GetComponent<CharacterInputHandler>();
+   
 
-    //    if (_characterInputHandler != null)
-    //    {
-    //        input.Set(_characterInputHandler.GetLocalInputs());
-    //    }
-    //}
-    
     public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
     {
         runner.Shutdown();
     }
-    
-    //private Transform PlayerSpawnPoint()
-    //{
-    //    Transform spawnPoint = null;
-    //    //var spawnPoints = FindObjectsOfType<SpawnPoint>();
 
-    //    //if (spawnPoints.Length == 0)
-    //    //{
-    //    //    Debug.LogError("No spawn points found!");
-    //    //    return null;
-    //    //}
+    private Transform PlayerSpawnPoint()
+    {
+        Transform spawnPoint = null;
+        var spawnPoints = FindObjectsOfType<SpawnPoints>();
 
-    //    //int offset = UnityEngine.Random.Range(0, spawnPoints.Length);
-    //    //for (int i = 0; i < spawnPoints.Length; i++)
-    //    //{
-    //    //    spawnPoint = spawnPoints[(offset + i) % spawnPoints.Length].transform;
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points found!");
+            return null;
+        }
 
-    //    //    if (!_recentSpawnPoints.Contains(spawnPoint))
-    //    //        break;
-    //    //}
+        int offset = UnityEngine.Random.Range(0, spawnPoints.Length);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            spawnPoint = spawnPoints[(offset + i) % spawnPoints.Length].transform;
 
-    //    //_recentSpawnPoints.Add(spawnPoint);
+            if (!_recentSpawnPoints.Contains(spawnPoint))
+                break;
+        }
 
-    //    //if (_recentSpawnPoints.Count > 2)
-    //    //{
-    //    //    _recentSpawnPoints.RemoveAt(0);
-    //    //}
+        _recentSpawnPoints.Add(spawnPoint);
 
-    //    //return spawnPoint;
+        if (_recentSpawnPoints.Count > 2)
+        {
+            _recentSpawnPoints.RemoveAt(0);
+        }
+
+        return spawnPoint;
 
 
 
-    //}
+    }
 
 
 
@@ -92,15 +81,12 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
     public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
     public void OnSceneLoadDone(NetworkRunner runner) { }
     public void OnSceneLoadStart(NetworkRunner runner) { }
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player){ }
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player){ }
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data){ }
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress){ }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
 
-    void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input)
-    {
-        throw new NotImplementedException();
-    }
+ 
 
     #endregion
 }
